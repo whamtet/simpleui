ae4389cc3dd30a34981d2e6604b757bd0c65f185
Matthew Molloy <whamtet@gmail.com>
2021-01-22 11:54:16 +0700

    move listeners to onEvent

--- resources/htmx_wrapper.js
+++ resources/htmx_wrapper.js
@@ -1,18 +1,18 @@
 var requestConfig;
 var toSwap;
 
-document.body.addEventListener('htmx:beforeRequest', function(info) {
-  requestConfig = info.detail.requestConfig;
-});
-
-document.body.addEventListener('htmx:beforeSwap', function(info) {
-  toSwap = eval(info.detail.xhr.response + '_static')(requestConfig);
-  if (toSwap === undefined) {
-    info.preventDefault();
-  }
-});
-
 htmx.defineExtension('static', {
+  onEvent : function(name, evt) {
+    if (name === 'htmx:beforeRequest') {
+      requestConfig = evt.detail.requestConfig;
+    }
+    if (name === 'htmx:beforeSwap') {
+      toSwap = eval(evt.detail.xhr.response + '_static')(requestConfig);
+      if (toSwap === undefined) {
+        evt.preventDefault();
+      }
+    }
+  },
   transformResponse: function(text, xhr, elt) {
     return toSwap;
   }
